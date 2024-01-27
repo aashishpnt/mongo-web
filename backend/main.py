4bc6566e2ca8c1f4451d4ef82424c2b1fa193f6f
Kaustub <kaustubniraula999@gmail.com>
2024-01-27 23:59:05 +0545

    api created login,register

--- backend/main.py
+++ backend/main.py
@@ -1,60 +1,84 @@
-# main.py
+from fastapi import FastAPI, HTTPException
+import torch
+import bcrypt
+import secrets
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from pydantic import BaseModel
-# from motor.motor_asyncio import AsyncIOMotorClient
+from bson import ObjectId
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 
+MONGO_URI="mongodb+srv://kaustubniraula999:g2cnkEI8yt9GkfaF@cluster0.hysfwcm.mongodb.net/"
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["mydatabase"] 
+Usercollection = db["users"]
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from pydantic import BaseModel
 
-uri = "mongodb+srv://aashishpnt:helloworld@cluster0.4rudedc.mongodb.net/?retryWrites=true&w=majority"
+# tokenizer = transformers.T5Tokenizer.from_pretrained("D:\\tokenizer")
+# model = transformers.T5ForConditionalGeneration.from_pretrained('D:\\model')
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+class UserDetail(BaseModel):
+    name: str = None
+    email: str
+    username: str = None
+    password : str
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-    
-    
-    
-# origins = ["http://localhost:3000"]  # Update with your React app's address
+async def get_all_emails():
+    emails = []
+    async for document in Usercollection.find({}, {"email": 1, "_id": 0}):
+        emails.append(document.get("email"))
+    return emails
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+@app.post("/users/register")
+async def register_user(user: UserDetail):
+    emails = await get_all_emails()
+    if user.email not in emails:
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), salt)
+        hashed_password_str = hashed_password.decode('utf-8')
+        salt_str = salt.hex()
+        
+        result = await Usercollection.insert_one({"name" : user.name,"username": user.username, "email": user.email, 
+                                                  "Password" : hashed_password_str , "Key" : salt_str})
+        return {"message": "User created successfully", "user_id": str(result.inserted_id)}
+    else:
+        return {"message": "Email already taken"}
 
-# @app.post("/signup")
-# async def signup(user_data: dict):  # Add appropriate validation with Pydantic model
-#     # Connect to MongoDB
-#     client = AsyncIOMotorClient("mongodb://localhost:27017")
-#     db = client["your_database_name"]
-#     users_collection = db["users"]
 
-#     # Perform signup logic (insert user_data into MongoDB)
-#     result = await users_collection.insert_one(user_data)
-#     return {"message": "User signed up successfully", "user_id": str(result.inserted_id)}
+@app.post("/users/login")
+async def login_user(user: UserDetail):
+    result = await Usercollection.find_one({"email": user.email})
+    if result:
+        stored_hashed_password = result['Password']
+        stored_random_key_str = result['Key']
+        stored_random_key_bytes = bytes.fromhex(stored_random_key_str)
+        hashed_input_password = bcrypt.hashpw(user.password.encode('utf-8'), stored_random_key_bytes)
 
-# @app.post("/login")
-# async def login(login_data: dict):  # Add appropriate validation with Pydantic model
-#     # Connect to MongoDB
-#     client = AsyncIOMotorClient("mongodb://localhost:27017")
-#     db = client["your_database_name"]
-#     users_collection = db["users"]
+        if hashed_input_password == stored_hashed_password.encode('utf-8'):
+            return {"message": "User authenticated", "user_id": str(result['_id'])}
+        else:
+            return {"message": "Authentication failed"}
+    else:
+        print("User not found")
 
-#     # Perform login logic (query MongoDB with login_data)
-#     user = await users_collection.find_one(login_data)
-#     if user:
-#         return {"message": "Login successful"}
-#     else:
-#         return {"message": "Invalid credentials"}
+
+@app.get("/users/get/{email}")
+async def get_user(email: str):
+    user = await Usercollection.find_one({"email": email},{'_id': 0})
+    if user:
+        return user
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
+@app.post("/")
+def GetUserQuery(str):
+#     input_text = [str]
+#     input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+#     output = model.generate(input_ids)
+
+#     output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    # return output_text
+    return 0
